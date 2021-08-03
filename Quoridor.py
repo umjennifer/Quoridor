2f05317326f96e7fbf21cb487480ab39683cd550
umjennifer <um.jennifer@gmail.com>
2021-08-02 20:05:51 -0700

    - changed board object to a dictionary with coordinates (tuples) as keys - added classes and functions

--- Quoridor.py
+++ Quoridor.py
@@ -6,41 +6,106 @@ class QuoridorGame:
     """
     TODO: description
     """
+
     def __init__(self):
-        self._pawn_board = self._generate_board("pawn")
-        self._fence_board = self._generate_board("fence")
 
-    def _generate_board(self, board_type):
-        board = []
-        if board_type == "pawn":
-            board_size = 10
-            board = [["[ ]" for i in range(board_size)] for j in range(board_size)]
-            for i in range(board_size):  # invalid row
-                board[i][board_size - 1] = " x "
-        else:  # for fences
-            board_size = 10
-            for i in range(board_size):
-                new_row = []
-                for j in range(board_size):
-                    new_row.append({"direction": "na", "coordinate": (j, i)})
-                board.append(new_row)
+        # initialize the board
+        self._game_status  # TODO: to determine winner
+        self._player1 = Player(1)
+        self._player2 = Player(2)
+        self._players = [self._player1, self._player2]  # TODO: maybe not needed
 
-            for j in range(board_size):
-                # first row is a fence
-                board[0][j]["direction"] = "h"
-                # last row is a fence
-                board[board_size-1][j]["direction"] = "h"
+        self._current_turn = self._player1
+        # self._player1_pawn = Pawn(self._player1.get_player_num())
 
-            for i in range(board_size):
-                # first row is a fence
-                board[i][0]["direction"] = "v"
-                board[i][board_size-1]["direction"] = "v"
+        # self._player2_pawn = Pawn(self._player2.get_player_num())
+        self._board = self._generate_board()
 
-            # we don't really care what the corners of this board are initalized with
+
+
+
+    def get_player_from_num(self, number):
+        for player in self._players:
+            if player.get_player_num() == number:
+                return player
+        return None
+
+    def _generate_board(self):
+        board = {}
+        for row in range(-1, 10):
+            for col in range(-1, 10):
+                board[(col, row)] = {"pawn": None, "fence": None}
+
+        # place player1 and player2 pawn in their starting positions
+        board[(4, 0)]["pawn"] = self._player1.get_player_num()
+        board[(4, 8)]["pawn"] = self._player2.get_player_num()
+
+        # edges are fences
+        for i in range(-1, 10):
+            board[(-1, i)]["fence"] = Fence("edge")
+            board[(9, i)]["fence"] = Fence("edge")
+            board[(i, -1)]["fence"] = Fence("edge")
+            board[(i, 9)]["fence"] = Fence("edge")
 
         return board
 
-    def move_pawn(self, player, desired_coord):
+    def _print_board(self):
+
+        # for key, value in self._board.items():
+        #     print(key, end=" ")
+        #     print()
+
+        for i in range(-1,10):
+            for j in range(-1,10):
+
+                this_coord = (j, i)
+
+                # create pawn string
+                if self._board[this_coord]["pawn"] is None:
+                    pawn_string = "None"
+                else:
+                    pawn_string = str(self._board[this_coord]["pawn"])
+
+                # create fence string
+                if self._board[this_coord]["fence"] is None:
+                    fence_string = "None"
+                else:
+                    fence_string = str(self._board[this_coord]["fence"].get_fence_direction())
+
+                location_string = str(this_coord) + ",pawn=" + pawn_string + ",fence=" + fence_string
+                print(location_string.ljust(35), end="")
+                # print(this_coord, self._board[this_coord], end=" | ")
+                # print(this_coord, end="")
+            print()
+        # for row in self._board:
+        #     for square in row:
+        #         for coord in square.keys():
+        #             print(coord.get_coordinate(), end="")
+        #         # for value in square.values():
+        #         #     print(value, end="")
+        #         # print(square.keys(), end="")
+        #     print()
+
+    def _get_current_turn(self):
+        return self._current_turn
+
+    def _change_current_turn(self):
+        if self._current_turn == self._player1:
+            self._current_turn = self._player2
+        else:
+            self._current_turn = self._player1
+        # if self._current_turn == 1:
+        #     self._current_turn = 2
+        # else:
+        #     self._current_turn = 1
+
+    def _validate_current_turn(self, inputted_player):
+        if inputted_player == self._current_turn:
+            return True
+        else:
+            return False
+
+    def move_pawn(self, player_num, desired_coord):
         """
         TODO:
         :param player:
@@ -52,9 +117,27 @@ class QuoridorGame:
             - or game has already been won
             True if desired move makes the player win
         """
-        pass
+        # check if player entered is the correct current player
+        if not self._validate_current_turn(player_num):  # TODO: put in function decorator
+            return False  # return False if invalid move
 
-    def place_fence(self, player, direction, desired_coord):
+
+
+        # check if player has fences left
+
+        # check if fence is within boundaries of the board
+
+        # check if another fence is already there and new fence will overlap or intersect with the fence
+
+        # if game has already won
+
+        # update current turn to the other player for the next round
+        self._change_current_turn()  # TODO: put in function decorator
+
+
+
+
+    def place_fence(self, player_num, direction, desired_coord):
         """
         TODO
         :param player:
@@ -69,8 +152,17 @@ class QuoridorGame:
             True if
             - fence can be placed there
         """
+        # check if player entered is the correct current player
+        if not self._validate_current_turn(player_num):  # TODO: put in function decorator
+            return False  # return False if invalid move
 
-    def is_winner(self, player):
+        # check if player has fences left
+        # if self.plaer
+
+        # update current turn to the other player for the next round
+        self._change_current_turn()  # TODO: put in function decorator
+
+    def is_winner(self, player_num):
         """
         TODO
         :return:
@@ -78,34 +170,64 @@ class QuoridorGame:
             False if that player has not won
         """
 
-    def print_board(self):
-        for i in range(10):
-            for j in range(10):
-                # print(self._fence_board[i][j]["direction"], end=" ")
-                direction = self._fence_board[i][j]["direction"]
-                coord = self._fence_board[i][j]["coordinate"]
 
-                special_coord = {(0, 0), (0, 9), (9, 0), (9, 9)}
+# class Board:
+#     def __init__(self):
+#         self._board = Boar
 
-                if coord in special_coord:
-                    print(" ╋ ", end="")
-                else:
-                    if direction == "h":
-                        print(" _ ", end="")
-                    elif direction == "v":
-                        print(" | ", end="")
-                    else:
-                        print(" . ", end="")
-                print(self._pawn_board[i][j], end="")
-            print()
+class Fence:
+    def __init__(self, direction):
+        # self._coord
+        self._direction = direction  # horizontal, vertical, or edge
 
-        # for i in range(10):
-        #     for j in range(10):
-        #         coord = self._pawn_board[i][j]
-        #         print(coord, end="")
-        #     print()
+    def get_fence_direction(self):
+        return self._direction
 
+
+class Pawn:
+    def __init__(self, player_num):
+        self._player = player_num
+
+    def get_pawn_number(self):
+        return self._player
+
+
+# class Coordinate:
+#     def __init__(self, col, row):
+#         self._col = col
+#         self._row = row
+#
+#     def get_coordinate(self):
+#         return (self._col, self._row)
+
+
+class Player:
+    def __init__(self, player_num):
+        self._player_num = player_num
+        self._num_fences_available = 10
+        # self._pawn = Pawn(1, (4, 8))
+        # # move_pawn(1, (4, 8))
+        # TODO: initialize first pawn on board
+
+    def get_player_num(self):
+        return self._player_num
+
+    def get_num_fences_available(self):
+        return self._num_fences_available
+
+    def decrement_num_fences_available(self):
+        self._num_fences_available -= 1
 
 
 q = QuoridorGame()
-q.print_board()
+q._print_board()
+# print(q.move_pawn(2, (4, 7)))  #moves the Player2 pawn -- invalid move because only Player1 can start, returns False
+# print(q.move_pawn(1, (4, 1)))  #moves the Player1 pawn -- valid move, returns True
+print(q.place_fence(1, 'h',(6,5)))
+# q.move_pawn(2, (4,7)) #moves the Player2 pawn -- valid move, returns True
+# q.place_fence(1, 'h',(6,5)) #places Player1's fence -- returns True
+# q.place_fence(2, 'v',(3,3)) #places Player2's fence -- returns True
+# q.is_winner(1) #returns False because Player 1 has not won
+# q.is_winner(2) #returns False because Player 2 has not won
+
+print(q.get_player_from_num(1))
